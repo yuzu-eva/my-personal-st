c5a18edd079589f400e72f5ec2962c354fe67283
yuzu-eva <stevenhu@web.de>
2022-11-25 13:07:17 +0100

    added library path to config.mk

--- config.mk
+++ config.mk
@@ -16,9 +16,14 @@ PKG_CONFIG = pkg-config
 INCS = -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+# LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+#        `$(PKG_CONFIG) --libs fontconfig` \
+#        `$(PKG_CONFIG) --libs freetype2`
+#
+LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender \
+	   -Wl,-rpath=/usr/local/lib \
        `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2`
+       `$(PKG_CONFIG) --libs freetype2` \
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
